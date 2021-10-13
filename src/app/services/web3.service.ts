dc8cfb555167b209153454967be07bac380ffa1d
InLearningPhase <taneshgupta2211@gmail.com>
2021-10-13 21:32:57 +0530

    update

--- src/app/services/web3.service.ts
+++ src/app/services/web3.service.ts
@@ -21,8 +21,8 @@ export class Web3Service {
 
   public async transferAmount(amount) {
     return await this.exhibition.methods.transferAmount().send({
-      from: this.accounts[0], 
-      value: this.web3.utils.toWei(amount, "ether") 
+      from: this.accounts[0],
+      value: this.web3.utils.toWei(amount, "ether")
     });
   }
 
@@ -58,10 +58,10 @@ export class Web3Service {
   // this method is used to create web3 object with MetaMask provider
   // create a exhibition contract instance
   private async createWeb3() {
-    // Checking if Web3 has been injected by the browser (MetaMask)
-    if (typeof window.web3 !== "undefined") {
-      // Use MetaMask's provider
-      this.web3 = new Web3(window.web3.currentProvider);
+
+    if (window.ethereum) {
+      this.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
 
       //create a exhibition contract instance
       this.exhibition = new this.web3.eth.Contract(
@@ -71,8 +71,21 @@ export class Web3Service {
 
       this.accounts = await this.web3.eth.getAccounts();
       console.log(this.accounts);
-    } else {
-      console.log("No web3? Please trying with MetaMask!");
+    }
+    else if (window.web3) {
+      this.web3 = new Web3(window.web3.currentProvider)
+
+      //create a exhibition contract instance
+      this.exhibition = new this.web3.eth.Contract(
+        this.interface, // contract interface
+        this.contractDeployedAt // address where contract is deployed
+      );
+
+      this.accounts = await this.web3.eth.getAccounts();
+      console.log(this.accounts);
+    }
+    else {
+      window.alert('Non ethereum browser detected. You should consider trying to install metamask')
     }
   }
 }
